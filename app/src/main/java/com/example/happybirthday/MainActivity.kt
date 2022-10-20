c03611ec9f77ade4bdaa1d09ae26d4918d36bdc4
Agus Adi <gusadi872@gmail.com>
2022-10-21 00:50:35 +0700

    finishing the card layout

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -9,10 +9,12 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -27,8 +29,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     BirthdayGreetingWithImage(
-                        message = "Happy Birtday Sam!",
-                        from = "- from Emma"
+                        name = "Sam",
+                        from = "Emma"
                     )
                 }
             }
@@ -37,7 +39,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthdayGreetingWithImage(message: String, from: String) {
+fun BirthdayGreetingWithImage(name: String, from: String) {
     val image = painterResource(id = R.drawable.androidparty)
 
     Box() {
@@ -49,22 +51,30 @@ fun BirthdayGreetingWithImage(message: String, from: String) {
                 .fillMaxWidth(),
             contentScale = ContentScale.Crop
         )
-        BirthdayGreetingWithText(message = message, from = from)
+        BirthdayGreetingWithText(name = name, from = from)
     }
 }
 
 @Composable
-fun BirthdayGreetingWithText(message: String, from: String) {
+fun BirthdayGreetingWithText(name: String, from: String) {
 
     Column() {
         Text(
-            text = message,
-            fontSize = 36.sp
+            text = "Happy Birthday $name!",
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, top = 16.dp)
         )
 
         Text(
-            text = from,
-            fontSize = 24.sp
+            text = "- from $from",
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, end = 16.dp)
         )
     }
 }
@@ -77,8 +87,8 @@ fun BirthdayGreetingWithText(message: String, from: String) {
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
         BirthdayGreetingWithImage(
-            message = "Happy Birtday Sam!",
-            from = "- from Emma"
+            name = "Sam",
+            from = "Emma"
         )
     }
 }
